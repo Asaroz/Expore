affb1dda9c9ac95437baf61682d88cecb628d276
andreglock <andreglock@gmail.com>
2021-11-04 14:59:22 +0100

    added README

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
 # Expore
+
 ## The custom world building application
 
-Build your own custom world.
+Build your own custom world:
+- Create continents, species, nations, atmospheres, or anything you like.
+- Make new objects or themes as subitems.
+- Create a giant tree of objects and information.
+- The only limit is your imagination! (and maybe our database bandwidth XD)
+
+## Resources
+
+- Backend: Express
+    - bcrypt
+    - nodemon
+    - dotenv
+    - mongoose
+- Frontend: React
+    - Bootstrap
+    - SASS
+- Database: MongoDB powered by Atlas
+- Deployment: https://expore.herokuapp.com/ (by Heroku)
+
+## Project
+
+https://github.com/Asaroz/Expore/projects/1
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { fileURLToPath } from 'url';
 import path, {dirname} from 'path';
+import { wildcardEndpoint, globalErrorHandler } from './controllers/fallbackController';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -19,6 +20,12 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 app.post('*', (req, res) => {
     res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
+
+// Wildcard endpoint, runs for everything that is not GET, except errors!
+app.use(wildcardEndpoint);
+
+// This middleware handles all uncaught errors
+app.use(globalErrorHandler);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on ${port}`));
