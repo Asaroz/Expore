e2232d09984680d9b1343481618ef640a05f8621
Asaroz <77623721+Asaroz@users.noreply.github.com>
2021-11-05 14:48:43 +0100

    login pre skeleton

---
+++ client/src/components/Login.jsx
@@ -0,0 +1,10 @@
+function Login(props){
+    setUser = props.setUser
+    // axios request should contain the login data from the form{ email/username: "test",password:"test",stayLogedIn:true/false} and setUser if login/register is succesfull
+    return <div>
+            <form>
+
+            </form>
+    
+    </div>
+}
